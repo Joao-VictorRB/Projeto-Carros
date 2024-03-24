a9a018017f87066f8df8f7a97825a860b75fff59
João Victor <joaovictor.zuca12@gmail.com>
2024-03-24 19:17:00 -0300

    instalação do Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors:{
+      'orange':'#28625',
+      'green':'#006971',
+      'dark-green':'#004140',
+      'offwhite':'rgba(255,255,255,0.75)',
+      'white':'#ffff',
+      'offwhitebg':'#f2f2f2'
+    }
+  },
+  plugins: [],
+}
