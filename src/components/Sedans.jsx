2dc1f27abca5ddf471d84fa2c9c244bf6f047b4c
João Victor <joaovictor.zuca12@gmail.com>
2024-03-24 22:14:28 -0300

    Projeto finalizado

--- src/components/Sedans.jsx
+++ src/components/Sedans.jsx
@@ -2,7 +2,7 @@ import iconSedans from "../assets/icon-sedans.svg"
 
 function Sedans() {
   return (
-    <div className="bg-orange p-12 max-lg:rounded-t-lg lg:rounded-l-lg">
+    <section className="bg-orange p-12 max-lg:rounded-t-lg lg:rounded-l-lg">
           
           <img src={iconSedans} alt="ícone de carrros sedan" className="mb-7" />         
           <h2 className="font-shoulders mb-7 uppercase text-4xl font-bold text-offwhitebg" >Sedans</h2>        
@@ -10,7 +10,7 @@ function Sedans() {
            e excelente economia de combustível. Ideal para dirigir na cidade ou na sua próxima viagem.</p>
           <a className="font-lexend bg-offwhitebg block px-10 py-3 w-fit rounded-3xl text-orange border-2 border-offwhitebg hover:bg-orange hover:text-offwhitebg" href="https://www.webmotors.com.br/sedans" target="_blank">Ver mais</a>
 
-    </div>
+    </section>
   )
 }
 
